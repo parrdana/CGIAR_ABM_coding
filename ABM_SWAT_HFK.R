bd26082e7b232448418ea5fcba4f12a6553706c3
parrdana <dparr3313@gmail.com>
2015-11-12 13:15:27 -0500

    Update ABM_SWAT_HFK.R

--- ABM_SWAT_HFK.R
+++ ABM_SWAT_HFK.R
@@ -14,7 +14,35 @@ while(n<22) #SWAT simulation period: 22 years
   {
   }
   
-  #Streamflow
+  ###################################################################################
+  #ABM output variables initialization (SWAT input variables)
+  #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  nosb<-47;nohru<-376;nores<-10;#number of subbasins, hru's, reservoirs
+  
+  initcrops<-read.table("Crop_initial.txt",header = FALSE, sep = "")
+  colnames(initcrops) <- c("SB_ID","HRU_ID","Area","PlantDate","PotIrri","Irri_TS","Irri_eff")
+  i_rice_area <- initcrops[,3][seq(1, length(initcrops[,3]), 8)]
+  r_rice_area <- initcrops[,3][seq(2, length(initcrops[,3]), 8)]
+  i_upl_area <- initcrops[,3][seq(3, length(initcrops[,3]), 8)]
+  r_upl_area <- initcrops[,3][seq(4, length(initcrops[,3]), 8)]
+  forest_area <- initcrops[,3][seq(5, length(initcrops[,3]), 8)]
+  grass_area <- initcrops[,3][seq(6, length(initcrops[,3]), 8)]
+  urban_area <- initcrops[,3][seq(7, length(initcrops[,3]), 8)]
+  wetland_area <- initcrops[,3][seq(8, length(initcrops[,3]), 8)]
+  
+  #for now all hru's are initialized with same irr_eff and all reservoirs with the same starg and ndtargr so no need to extract data
+   res_ini <- read.csv(file="resini.csv")
+  irr_eff<-vector(,nohru) 
+  irr_eff[]<-0.5
+  starg<-matrix(nrow=nores, ncol=12)
+  starg[]<-99000
+  ndtargr<-vector(,nores)
+  ndtargr[]<-15
+  
+  
+  #######################################################################################################
+  #SWAT output variables (ABM input variables)
+  ###### Streamflow 
   flow_mekong <- read.table("Flow_Mekong.txt")
   colnames(flow_mekong)[1:49] <- c("year","cal_day",paste0("Flow_SB_",1:47))
   
@@ -24,7 +52,7 @@ while(n<22) #SWAT simulation period: 22 years
     mutate(SB_ID = extract_numeric(Subbasin)) %>%
     select(-Subbasin)
   
-  ####### Storage, outflow and surface area########
+  ####### Reservoir Storage, outflow and surface area
   reservoir_mekong <- read.table("Reservoir_Mekong.txt")
   colnames(reservoir_mekong)[1:32] <- c("year","cal_day",paste0("Volume_Res",1:10),paste0("SurfArea_Res",1:10),paste0("Outflow_Res",1:10))
   
@@ -34,18 +62,9 @@ while(n<22) #SWAT simulation period: 22 years
     separate(col=Att,into=c("Attr","Reservoir"),sep="_") %>%
     mutate(Reservoir = extract_numeric(Reservoir)) 
   
-  #Crop yield
+  ####### Crop yield
   crop_mekong  <- read.table("Crop_Mekong.txt")
   colnames(crop_mekong) <- c("year","SB_ID","HRU_ID","yield","IWW")
-
-  res_ini <- read.csv(file="resini.csv")
-  
-  crop_hru <- read.table("Crop_initial.txt")
-  colnames(crop_hru) <- c("SB_ID","HRU_ID","Area","PlantDate","PotIrri","Irri_TS","Irri_eff")
-  
-  #min_cy: this will be a dataframe that provides a minimum constraint on crop yield for each HRU where there is cropping
-  #min_cprod: this will be a dataframe that provides a minimum constraint on crop production for each HRU where there is cropping
-  #min_hydpow: this will be a dataframe that provides a minimum constraint on hydropower for each reservoir
   
   ###############################
   #Upper level constraints
@@ -53,6 +72,9 @@ while(n<22) #SWAT simulation period: 22 years
   #DoA
   #DoEnv
   
+  #min_cy: this will be a dataframe that provides a minimum constraint on crop yield for each HRU where there is cropping
+  #min_cprod: this will be a dataframe that provides a minimum constraint on crop production for each HRU where there is cropping
+  #min_hydpow: this will be a dataframe that provides a minimum constraint on hydropower for each reservoir
   
   ################################
   #Post-calculation for (From SWAT output) ecosystem services
@@ -124,13 +146,11 @@ while(n<22) #SWAT simulation period: 22 years
 ############################################################################################################################
 ############################################################################################################################
 
-  irr_eff<-rep(0.8,376) #assign values to irrigation efficieny
-  starg<-matrix(data = 99000,nrow=10, ncol=12) #target
-  x<-cbind(starg,rep(15,10))
-  
-  # write new managment parameters to files
+  res<-cbind(starg,ndtargr)
+  land_area<-cbind(i_rice_area,r_rice_area,i_upl_area,r_upl_area,forest_area,grass_area,urban_area,wetland_area)
   write.table(irr_eff,file="Irr_eff_by_R.txt",col.names = F, row.names = F)
-  write.table(x,file="Reservoir_by_R.txt", col.names = F, row.names = F) 
+  write.table(res,file="Reservoir_by_R.txt", col.names = F, row.names = F) 
+  write.table(land_area,file="Landclass_Area_by_R.txt", col.names = F, row.names = F) 
   
   file.create("SWAT_flag.txt")
   n<-n+1
